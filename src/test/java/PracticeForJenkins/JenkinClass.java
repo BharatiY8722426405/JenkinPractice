9f4298d849351fa7339f8b28509258cc2a0f4561
BharatiY8722426405 <bharatiyaligar1996@gmail.com>
2022-12-05 16:27:57 +0530

    Push commit

---
+++ src/test/java/PracticeForJenkins/JenkinClass.java
@@ -0,0 +1,13 @@
+package PracticeForJenkins;
+
+import org.testng.annotations.Test;
+
+public class JenkinClass {
+	@Test
+	public void BuildCreation()
+	{
+		System.out.println("jenkins");
+		System.out.println("build creation");
+	}
+
+}
---
+++ src/test/java/PracticeForJenkins/JenkinClass4.java
@@ -0,0 +1,20 @@
+package PracticeForJenkins;
+
+import org.testng.annotations.Test;
+
+public class JenkinClass4 {
+	@Test
+	public void BuildCreation()
+	{
+		System.out.println("jenkins");
+		System.out.println("build creation");
+	}
+	@Test
+	public void Builddeploy()
+	{
+		System.out.println("build");
+		System.out.println("build2");
+		
+	}
+
+}
